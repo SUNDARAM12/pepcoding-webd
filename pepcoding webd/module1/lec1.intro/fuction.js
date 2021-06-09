4074ce606e855781435e0aa79fa693e6dfa5f090
sundaram <sundaram12062000@gmail.com>
2021-06-09 22:44:05 +0530

    first function

---
+++ pepcoding webd/module1/lec1.intro/fuction.js
@@ -0,0 +1,13 @@
+function callMe(name){
+    console.log("call me function called!!");
+    console.log(name + "hii h r u?");
+    return 20;
+}
+
+//function invocation 
+callMe();
+let s=callMe();
+console.log(s);
+
+
+callMe("steve")
